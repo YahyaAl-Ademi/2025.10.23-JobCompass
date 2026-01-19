b90e1f95e5581955c53fc7b3f235b9be07835799
Yaroslav <1.yaroslav.kazeev@gmail.com>
2026-01-19 05:51:31 +0100

    Update search_strings table to change is_auth type from boolean to uuid

--- db_migrations/create_tables.sql
+++ db_migrations/create_tables.sql
@@ -45,7 +45,7 @@ CREATE TABLE IF NOT EXISTS user_favorites (
 CREATE TABLE IF NOT EXISTS search_strings (
     search_string text NOT NULL,
     search_date timestamp without time zone,
-    is_auth boolean DEFAULT false,
+    is_auth uuid,
     CONSTRAINT search_strings_pkey PRIMARY KEY (search_string)
 );
 
