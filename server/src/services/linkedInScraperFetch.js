bc631090b956d2d2d6e51f61cbe418400e559bc2
Yaroslav <1.yaroslav.kazeev@gmail.com>
2026-01-21 14:17:44 +0100

    fix: update default location in linkedInScraperFetch to Drenthe

--- server/src/services/linkedInScraperFetch.js
+++ server/src/services/linkedInScraperFetch.js
@@ -9,7 +9,7 @@ if (!process.env.LINKEDIN_SCRAPER_KEY) {
 
 export default async function linkedInScraperFetch(
   search_string,
-  location = "Netherlands",
+  location = "Drenthe",
 ) {
   const aggregated = [];
   const token = process.env.LINKEDIN_SCRAPER_KEY;
