f3a81e2a941619434af8ddb6f0150e4fb84cb925
Yaroslav <1.yaroslav.kazeev@gmail.com>
2026-01-25 21:48:45 +0100

    refactor: remove scraperFetchPersister service to simplify job fetching process

--- server/src/controllers/jobData.js
+++ server/src/controllers/jobData.js
@@ -1,7 +1,6 @@
 import { logError } from "../util/logging.js";
 import connectNeonDB from "../db/connectNeonDB.js";
 import getCachedJobsBySearchString from "../services/getCachedJobsBySearchString.js";
-import scraperFetchPersister from "../services/ScraperFetchPersister.js";
 import rapidAPIfetchPersister from "../services/rapidAPIfetchPersister.js";
 
 export default async function searchJobs(req, res) {
