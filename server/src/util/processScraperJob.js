ea11d5db3b2b11464c7d138f08d23b5d3982f17c
Yaroslav <1.yaroslav.kazeev@gmail.com>
2026-02-24 15:45:20 +0100

    Refactor HTML tag handling: introduce replaceHtmlTagsWithSpaces utility for improved text normalization

--- server/src/util/processScraperJob.js
+++ server/src/util/processScraperJob.js
@@ -4,6 +4,13 @@ import normalizeUrl, { stripRefIdFromUrl } from "./normalizeUrl.js";
 import checkExperienceLevel from "./checkExperienceLevel.js";
 import detectLanguage from "./detectLanguage.js";
 
+function replaceHtmlTagsWithSpaces(text) {
+  if (typeof text !== "string" || text.length === 0) {
+    return "";
+  }
+  return text.replace(/<[^>]*>/g, " ");
+}
+
 export default function processScraperJob(job) {
   const {
     applyUrl: url1,
@@ -32,7 +39,10 @@ export default function processScraperJob(job) {
 
   const url = stripRefIdFromUrl(normalizeUrl(url1) || normalizeUrl(url2));
   const normalized_description =
-    normalizeText(title) + normalizeText(descriptionText);
+    normalizeText(title) +
+    normalizeText(
+      replaceHtmlTagsWithSpaces(description_text || descriptionText),
+    );
 
   const processedJob = {
     id: url,
