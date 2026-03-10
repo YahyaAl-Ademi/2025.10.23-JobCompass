07807e6dbcb9b2dc7a5baffcf605021c9dc05b93
Yaroslav <1.yaroslav.kazeev@gmail.com>
2026-03-10 14:07:43 +0100

    fix: update job deletion logic in cleanupDatabase to exclude jobs not in search_strings_jobs

--- server/src/services/cleanupDatabase.js
+++ server/src/services/cleanupDatabase.js
@@ -10,7 +10,7 @@ export default async function cleanupDatabase() {
         "DELETE FROM search_strings WHERE search_date < NOW() - INTERVAL '1 week'",
       );
       await connectedClient.query(
-        "DELETE FROM jobs WHERE date_posted < NOW() - INTERVAL '1 month' OR (date_posted < NOW() - INTERVAL '1 week' AND id NOT IN (SELECT job_id FROM user_favorites))",
+        "DELETE FROM jobs WHERE date_posted < NOW() - INTERVAL '1 month' OR (id NOT IN (SELECT job_id FROM search_strings_jobs) AND id NOT IN (SELECT job_id FROM user_favorites))",
       );
       await connectedClient.query(
         "DELETE FROM search_strings_jobs WHERE job_id NOT IN (SELECT id FROM jobs) OR search_string NOT IN (SELECT search_string FROM search_strings)",
