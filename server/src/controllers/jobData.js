393559f170e78e9c1013a57b615f05d342f3f95f
Yaroslav <1.yaroslav.kazeev@gmail.com>
2026-04-26 20:00:44 +0200

    refactor(cleanupDatabase): streamline error handling and improve connection logic

--- server/src/controllers/jobData.js
+++ server/src/controllers/jobData.js
@@ -17,81 +17,81 @@ export default async function searchJobs(req, res, next) {
   if (connectionError) {
     logError(`DB Connection Error: ${connectionError}`);
     return next(createHttpError(503, "DB Connection Error"));
-  } else {
-    try {
-      let { search_string } = req.body;
-      let aggregatedJobs = [];
-      const aggregatedJobsIdsSet = new Set();
+  }
 
-      if (typeof search_string !== "string" || !search_string.trim()) {
-        return next(
-          createHttpError(
-            400,
-            "You need to provide 'search_string' (non-empty string) in the request body.",
-          ),
-        );
-      }
+  try {
+    let { search_string } = req.body;
+    let aggregatedJobs = [];
+    const aggregatedJobsIdsSet = new Set();
 
-      search_string = search_string.toLowerCase();
-      const { is_whole_string, cachedJobsPerSearchString } =
-        await getCachedJobsBySearchString(
-          connectedClient,
-          search_string,
-          is_auth,
-        );
+    if (typeof search_string !== "string" || !search_string.trim()) {
+      return next(
+        createHttpError(
+          400,
+          "You need to provide 'search_string' (non-empty string) in the request body.",
+        ),
+      );
+    }
 
-      if (cachedJobsPerSearchString.length > 0) {
-        cachedJobsPerSearchString.forEach((job) => {
-          aggregatedJobs.push(job);
-          if (job.id) aggregatedJobsIdsSet.add(job.id);
-        });
-      }
-
-      const msg = apifyScraperFetchPersister(
+    search_string = search_string.toLowerCase();
+    const { is_whole_string, cachedJobsPerSearchString } =
+      await getCachedJobsBySearchString(
+        connectedClient,
         search_string,
-        is_whole_string,
         is_auth,
       );
 
-      const searchWords = search_string.split(/[\s\-/]+/).filter(Boolean);
-      // If the DB has already responded with the job title which is the single keyword, there's no need to break it down further and re-query for the same keyword.
-      if (is_whole_string === undefined || searchWords.length > 1) {
-        for (const searchWord of searchWords) {
-          const cachedResult = await getCachedJobsBySearchString(
-            connectedClient,
-            searchWord,
-            is_auth,
-          );
-          let fetchedJobs = [];
-          if (cachedResult.cachedJobsPerSearchString.length > 0) {
-            fetchedJobs = cachedResult.cachedJobsPerSearchString;
-          } else {
-            fetchedJobs = await rapidAPIfetchPersister(searchWord, is_auth);
-          }
+    if (cachedJobsPerSearchString.length > 0) {
+      cachedJobsPerSearchString.forEach((job) => {
+        aggregatedJobs.push(job);
+        if (job.id) aggregatedJobsIdsSet.add(job.id);
+      });
+    }
 
-          for (const job of fetchedJobs) {
-            if (job.id && !aggregatedJobsIdsSet.has(job.id)) {
-              aggregatedJobs.push(job);
-              aggregatedJobsIdsSet.add(job.id);
-            }
+    const msg = apifyScraperFetchPersister(
+      search_string,
+      is_whole_string,
+      is_auth,
+    );
+
+    const searchWords = search_string.split(/[\s\-/]+/).filter(Boolean);
+    // If the DB has already responded with the job title which is the single keyword, there's no need to break it down further and re-query for the same keyword.
+    if (is_whole_string === undefined || searchWords.length > 1) {
+      for (const searchWord of searchWords) {
+        const cachedResult = await getCachedJobsBySearchString(
+          connectedClient,
+          searchWord,
+          is_auth,
+        );
+        let fetchedJobs = [];
+        if (cachedResult.cachedJobsPerSearchString.length > 0) {
+          fetchedJobs = cachedResult.cachedJobsPerSearchString;
+        } else {
+          fetchedJobs = await rapidAPIfetchPersister(searchWord, is_auth);
+        }
+
+        for (const job of fetchedJobs) {
+          if (job.id && !aggregatedJobsIdsSet.has(job.id)) {
+            aggregatedJobs.push(job);
+            aggregatedJobsIdsSet.add(job.id);
           }
         }
       }
-
-      return res.status(200).json({
-        success: true,
-        result: aggregatedJobs,
-        msg,
-      });
-    } catch (error) {
-      return next(
-        createHttpError(
-          500,
-          "Unable to search for jobs, please try again later.",
-        ),
-      );
-    } finally {
-      if (endConnection) await endConnection();
     }
+
+    return res.status(200).json({
+      success: true,
+      result: aggregatedJobs,
+      msg,
+    });
+  } catch (error) {
+    return next(
+      createHttpError(
+        500,
+        "Unable to search for jobs, please try again later.",
+      ),
+    );
+  } finally {
+    if (endConnection) await endConnection();
   }
 }
